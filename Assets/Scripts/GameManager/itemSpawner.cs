8b81dceddad546ec7d1c1aba1d6d9b2875bfe885
abhi6403 <163630847+abhi6403@users.noreply.github.com>
2024-11-25 14:22:22 +0530

    Implemented obstacle

---
+++ Assets/Scripts/GameManager/itemSpawner.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemSpawner : MonoBehaviour
+{
+    [SerializeField] private float minSpawnTime;
+    [SerializeField] private float maxSpawnTime;
+
+    public BoxCollider2D spawnBoundary;
+
+    public GameObject[] itemsList;
+
+    bool isSpawning = false;
+
+    private void Update()
+    {
+        if (!isSpawning)
+        {
+            isSpawning = true;
+            StartCoroutine(SpawnItem(UnityEngine.Random.Range(minSpawnTime,maxSpawnTime)));
+        }
+    }
+
+    IEnumerator SpawnItem(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        Vector3 pos = GetRandomPosition();
+
+        int numberOfObjects = Random.Range(0, itemsList.Length);
+        GameObject cloud = Instantiate(itemsList[numberOfObjects], pos, Quaternion.identity);
+        isSpawning = false;
+    }
+
+    private Vector3 GetRandomPosition()
+    {
+        Bounds bounds = spawnBoundary.bounds;
+        float yPos = UnityEngine.Random.Range(bounds.min.y, bounds.max.y);
+
+        return new Vector3(transform.position.x , yPos, 0f);
+    }
+}
---
+++ Assets/Scripts/Obstacle/ObstacleScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class ObstacleScript : MonoBehaviour
+{
+    public float moveSpeed;
+    Vector3 moveRight;
+
+    private void Start()
+    {
+        moveRight = new Vector3(10.0f,transform.position.y,0.0f);
+    }
+
+    private void Update()
+    {
+        BulletMovement();
+        DestroyBullet();
+    }
+    private void BulletMovement()
+    {
+        var step = moveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, moveRight, step);
+    }
+
+    private void DestroyBullet ()
+    {
+        if (transform.position.x == 10.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+        }
+        else
+        {
+             
+            Destroy(gameObject);
+        }
+    }
+}
