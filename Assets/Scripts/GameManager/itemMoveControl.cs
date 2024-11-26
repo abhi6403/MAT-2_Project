1b0720680011c3bad0a9a00e62585749061e6f2e
abhi6403 <163630847+abhi6403@users.noreply.github.com>
2024-11-26 13:37:42 +0530

    Implemented Power ups

--- Assets/Scripts/GameManager/itemMoveControl.cs
+++ Assets/Scripts/GameManager/itemMoveControl.cs
@@ -1,30 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class ObstacleScript : MonoBehaviour
+public class itemMoveControl : MonoBehaviour
 {
     public float moveSpeed;
     Vector3 moveRight;
 
     private void Start()
     {
-        moveRight = new Vector3(10.0f,transform.position.y,0.0f);
+        moveRight = new Vector3(10.0f, transform.position.y, 0.0f);
     }
 
     private void Update()
     {
-        BulletMovement();
-        DestroyBullet();
+        ItemMovement();
+        DestroyItem();
     }
-    private void BulletMovement()
+    private void ItemMovement()
     {
         var step = moveSpeed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, moveRight, step);
     }
 
-    private void DestroyBullet ()
+    private void DestroyItem()
     {
         if (transform.position.x == 10.0f)
         {
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -10,11 +11,15 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D playerRb;
     private Animator playerAnimator;
+    private BoxCollider2D playerCollider;
+
+    bool isShieldActive = false;
 
     private void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
         playerAnimator = GetComponent<Animator>();
+        playerCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
@@ -25,9 +30,27 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Cloud"))
         {
-            playerRb.AddForce(new Vector2(0,jumpForce), ForceMode2D.Impulse);
+            playerRb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             playerAnimator.SetBool("Jump", true);
-        } else
+        }
+        else if (collision.gameObject.CompareTag("Jet"))
+        {
+            Destroy(collision.gameObject);
+            Debug.Log("getting called");
+            StartCoroutine(JetPower());
+        }
+        else if(collision.gameObject.CompareTag("Shield") && isShieldActive == false)
+        {
+            Destroy(collision.gameObject);
+            Debug.Log("getting called");
+            StartCoroutine(ShieldPower());
+        }
+        else if(collision.gameObject.CompareTag("Bullet") && isShieldActive == false)
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+        }
+        else
         {
             playerAnimator.SetBool("Jump", false);
         }
@@ -37,5 +60,21 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 mouseposition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector2(mouseposition.x, transform.position.y);
+    }
+
+    private IEnumerator JetPower()
+    {
+        playerCollider.isTrigger = true;
+        playerRb.constraints = RigidbodyConstraints2D.FreezePositionY;
+        yield return new WaitForSeconds(5);
+        playerRb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        playerCollider.isTrigger = false;
+    }
+
+    private IEnumerator ShieldPower()
+    {
+        isShieldActive = true;
+        yield return new WaitForSeconds(5);
+        isShieldActive = false;
     }
 }
